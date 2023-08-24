71db94e157962da3971a0c6af288aceb72275782
Santiago <sanct1318@gmail.com>
2023-08-24 18:29:12 -0500

    Creación del servidor en NODE

---
+++ index.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+
+const app = express();
+
+app.use(bodyParser.json());
+
+const port = 3000;
+
+app.listen(port, () => console.log("http://localhost:3000"));
+
+app.get("/", (req,res) =>{
+    res.send("Hola Andrea");
+});
+
+app.get("/test/:nombre", (req,res) =>{
+
+    res.send("test " + req.params.nombre);
+});
