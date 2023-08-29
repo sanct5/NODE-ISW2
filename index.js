2d9ee40f708097d930e4b29118838abd8f03ebcc
sanct5 <130994767+sanct5@users.noreply.github.com>
2023-08-29 14:01:01 -0500

    Agregar método post

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.use(bodyParser.json());
 
 const port = 3000;
 
-app.listen(port, () => console.log("http://localhost:3000"));
+app.listen(port, () => console.log("http://localhost:10533"));
 
 app.get("/", (req,res) =>{
     res.send("Hola Andrea");
@@ -16,4 +16,10 @@ app.get("/", (req,res) =>{
 app.get("/test/:nombre", (req,res) =>{
 
     res.send("test " + req.params.nombre);
+});
+
+app.post("/testpost", (req, res) => {
+    const data = req.body;
+    console.log("Datos recibidos:", data);
+    res.send("OK");
 });
